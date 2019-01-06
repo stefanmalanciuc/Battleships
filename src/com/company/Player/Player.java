641845fa65b4bb278f9601ed68b914ad981ceabd
Denisneagoe <neagoedenis@yahoo.com>
2019-01-06 12:24:59 +0200

    Player

---
+++ src/com/company/Player/Player.java
@@ -0,0 +1,23 @@
+package com.company.Player;
+
+public class Player {
+
+    private String PlayerName;
+
+    public Player(String name) {
+        PlayerName = name;
+    }
+
+    public void setName(String name) {
+        this.PlayerName = name;
+    }
+
+    public String getName() {
+        return PlayerName;
+    }
+
+    private int Score;{
+
+
+    }
+}
